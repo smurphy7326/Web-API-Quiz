ec172fc2a6acb7af0766c393e5fe067664c6e077
Your Name <you@example.com>
2021-08-19 17:33:52 -0400

    debugged to get questions

--- javascript.js
+++ javascript.js
@@ -3,9 +3,9 @@
 
 var questionsEl = document.querySelector("#questions");
 var timerEl = document.querySelector("#time");
-var choicesEL = document.querySelector("#choices");
+var choicesEl = document.querySelector("#choices");
 var submitBtn = document.querySelector("#submit");
-var startBtn = document.querySelector("#start");
+var startBtn = document.querySelector("#startbtn");
 var initialsEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
 
