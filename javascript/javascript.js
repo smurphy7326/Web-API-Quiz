fee9ba32893aa85256f77b1d63afefda1e7e3083
Your Name <you@example.com>
2021-08-19 13:39:02 -0400

    CSS and folders

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -18,42 +18,43 @@ var timerId;
 function startQuiz() {
     var startScreenEl = document.getElementById("start-screen");
     startScreenEl.setAttribute("class", "hide");
+
+    //unhide questions section
+    questionsEl.removeAttribute("class");
+
+    //Start Timer Section
+    timerID = setAttribute(clockTick, 1000);
+
+    //Show starting Time
+    timerEl.textContent = time;
+    getQuestion();
 }
 
-//unhide questions section
-questionsEl.removeAttribute("class");
+    function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex]; 
 
-//Start Timer Section
-timerID = setAttribute(clockTick, 1000);
-
-//Show starting Time
-timerEl.textContent = time;
-
-function getQuestion() {
-    var currentQuestion = questions[currentQuestionIndex];
-}
-
-//
-var titleEl = document.getElementById("question-title")
+    // Converts title to question
+    var titleEl = document.getElementById("question-title")
     titleEl.textContent = currentQuestion.title;
 
-//Clear out old questions so they are not asked again
-choicesEl.innerHTML = "";
+    //Clear out old questions so they are not asked again
+    choicesEl.innerHTML = "";
 
-// Create a loop
-currentQuestion.choices.forEach(function(choice, i) {
-    var choiceNode = document.createElement("button");
-    choiceNode.setAttribute("class", "choice");
-    choiceNode.setAttribute ("value", choice);
-}
+    // Create a loop
+    currentQuestion.choices.forEach(function(choice, i) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute ("value", choice);
 
     choiceNode.textContent = i + 1 + ". " + choice;
 
     choicesEl.appendChild(choiceNode);
-});
+  });
+}
 
 function questionClick() {
-    if (this.value ! == questions[currentQuestionIndex].answer) {
+    // if the user guessed wrong...
+    if (this.value !== questions[currentQuestionIndex].answer) {
         time -=15;
 
         if (time < 0) {
@@ -141,3 +142,10 @@ function checkForEnter(event) {
     }
 }
 
+// submit initials
+submitBtn.onclick = saveHighscore;
+
+// start Quiz button
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
