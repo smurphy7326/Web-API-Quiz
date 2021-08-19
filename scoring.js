dbddf9b6975010790ad52d72cb31e229b348f238
Your Name <you@example.com>
2021-08-19 14:21:36 -0400

    bookkeeping

---
+++ scoring.js
@@ -0,0 +1,28 @@
+//Scoring Aspect
+function printHighScores() {
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+}
+
+// How to get the scores in proper order
+highscores.sort(function(a, b) {
+    return b.score - a.score;
+});
+
+// Come back to this and make sure it is correct 
+highscores.forEach(function(score) {
+    var liTag = doc.createElement("li");
+    liTag.textContent = score.initials + " - " + score.score;
+
+// show it on the page
+    var olEL = document.getElementById("highscores");
+    olEl.appendChild(liTag);
+    });
+
+function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+}
+
+document.getElementById("clear").onclick = clearHighscores;
+
+printHighScores();
