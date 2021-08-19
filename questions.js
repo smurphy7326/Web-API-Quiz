7794b57a0e9151b1af297e565abe924cbbee0bca
Your Name <you@example.com>
2021-08-19 12:15:43 -0400

    more questions

--- questions.js
+++ questions.js
@@ -7,7 +7,60 @@ var questions = [
         answer: "body"
     },
 
+    {
+     title: "What does HTML stand for?",
+     choices: ["Hyper Tool Markup Language", "Hyper Text Makeup Language", "Hyper Text Markup Language", "Home Text Markup Language"],
+     answer: "Hyper Text Markup Language"   
+    },
+
+    {
+    title: "Choose the correct HTML tag for the largest heading",
+    choices: ["h4", "h6", "h2", "h1"],
+    answer: "h1"
+    },
+
+    {
+    title: "What does CSS stand for?",
+    choices: ["Cascading Style Sheets", "Cascading Style Score", "Continuing Style Sheets", "Style Cascading Sheets"],
+    answer: "Cascading Style Sheets"
+    },
+
+    {
+    title: "Which of the following is used to determine the heoght of an image?",
+    choices: ["Width", "Height", "Border", "Hover"],
+    answer: "Height"
+    },
+
+    {
+    title: "Which CSS property is used to control the elements of font-size?",
+    choices: ["font-size", "font-height", "font-color", "none of the above"],
+    answer: "font-size"
+    },
+
+    {
+    title:
+    choices:
+    answer:
+    }
 
 
+    {
+    title:
+    choices:
+    answer:
+    }
 
+
+    {
+    title:
+    choices:
+    answer:
+    }
+
+
+    {
+    title:
+    choices:
+    answer:
+    }
 ]
